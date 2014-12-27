710af7f7826f2116737ba88692af75afd26b36e3
epond <epond@users.noreply.github.com>
2014-12-27 16:34:13 +0000

    Experiment: got request body out

--- src/Experiment.hs
+++ src/Experiment.hs
@@ -1,9 +1,8 @@
 module Main where
 
 import Control.Monad
-import Happstack.Server
-import Happstack.Server.Types
 import Data.Char (toLower)
+import Happstack.Server
 
 -- The simpleHTTP function listens for HTTP requests. Here is its type:
 -- simpleHTTP :: (ToMessage a) => Conf -> ServerPartT IO a -> IO ()
@@ -21,8 +20,6 @@ main = simpleHTTP (nullConf { port = 9000}) $ msum
     , seeOther "" "Request not recognised"
     ]
 
--- Where should takeRequestBody go?
--- takeRequestBody :: MonadIO m => Request -> m (Maybe RqBody)
 
 data Subject = World | Haskell
 
@@ -49,7 +46,15 @@ greetingsHandler = path $ \x -> ok $ "Greetings to " ++ x
 
 -- Try adding askRq to get request then takeRequestBody to get the request body
 -- askRq :: m Request
+-- takeRequestBody :: MonadIO m => Request -> m (Maybe RqBody)
 echoHandler :: ServerPart String
 echoHandler =
-    do request <- askRq
-       ok $ "echo " ++ (rqUri request)
+    do request <- askRq                     -- :: Request
+       maybeBody <- takeRequestBody request -- :: Maybe RqBody
+       ok $ "echo " ++ bodyToString maybeBody
+
+bodyToString :: Maybe RqBody -> String
+bodyToString body =
+    case body of
+        Just rqbody -> "found body: " ++ (show . unBody $ rqbody)
+        Nothing     -> "no body"
