74308898fc40ed34fa1e0caa97a2032a961c5346
Ed Pond <edward.pond@springer.com>
2015-01-09 14:31:52 +0000

    More Hspec examples

--- test/ExampleSpec.hs
+++ test/ExampleSpec.hs
@@ -4,10 +4,18 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "Prelude.head" $ do
-    it "returns the first element of a list" $ do
-      head [23 ..] `shouldBe` (23 :: Int)
+  describe "The thing under test" $ do
+    context "You can describe additional context here" $ do
+      it "can be tested using equality" $ do
+        head [23 ..] `shouldBe` (23 :: Int)
+      it "can be tested using a predicate" $ do
+        [1,2,3] `shouldSatisfy` (not . null)
+      it "does something that is not yet implemented" $ do
+        pending
+      it "does something that is not yet implemented, with explanation" $ do
+        pendingWith "this feature needs x"
 
 
 main :: IO ()
 main = hspec spec
+
